ffb60054e0e6615e88fe13b471de9051d9bf0132
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-05-29 20:20:54 +0100

    Typed function with default value

--- index.ts
+++ index.ts
@@ -7,6 +7,8 @@ const addLambda = (a: number, b: number) => a + b
 const sum = add(3, 4);
 const sumLambda = addLambda(3, 4)
 
-const multiply = (a: number, b?: number) => a * (b ?? 2);
+const multiply = (a: number, b?: number) => a * (b ?? 2)
+const multiplyWithDefault = (a: number, b: number = 2) => a * b
 
 console.log(multiply(3))
+console.log(multiplyWithDefault(3))
