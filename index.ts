6b8450d3c77248041601d7bfbcbd1d96d7e8eb3f
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-05-29 19:41:51 +0100

    Tuples

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
-type NumberOrArrayOfStrings = number | string[]
+type NumberOrArrayOfStrings = number | string[];
 
 let reusableVariable: NumberOrArrayOfStrings = 30;
 
 reusableVariable = ['hello'];
 
+type NumberAndStringTupleType = [number, string]
 
+let tupleVariable: NumberAndStringTupleType;
+
+tupleVariable = [1, 'goodbye'];
+
+type Triple = [number, string, string[]];
+
+let tripleVariable: Triple;
+
+tripleVariable = [1, 'whoop', ['first', 'second', 'third']]
